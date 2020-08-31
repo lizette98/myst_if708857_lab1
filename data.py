14d7900ec284f72fd20e0c9863a423fc367f4010
lizette98 <andreal98.acg@gmail.com>
2020-08-31 13:15:10 -0500

    aug31_changes

--- data.py
+++ data.py
@@ -8,6 +8,8 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+
 import pandas as pd
 from os import listdir, path
 from os.path import isfile, join
@@ -16,6 +18,9 @@ import yfinance as yf
 import time as time
 
 pd.set_option('display.expand_frame_rep', False)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
 # Leer archivos
 abspath = path.abspath('files/NAFTRAC_holdings')
@@ -30,25 +35,20 @@ data_archivos = {}
 
 for i in archivos:
     i = archivos[0]
-
     #Los lee después de los primeros dos renglones
-    data = pd.read_csv('files/NAFTRAC_holdings/'+ archivos[0] + '.csv', skiprows=2, header=None)
-
+    data = pd.read_csv('files/NAFTRAC_holdings/'+ i + '.csv', skiprows=2, header=None)
     #Renombrar columnas
     data.columns = list(data.iloc[0, :])
-
     #Quitar columnas que no sean nan
     data = data.loc[:, pd.notnull(data.columns)]
     #Quitar renglon repetido y resetear índice, que empiece en 0
-    data = data.iloc[1:-1].reset_index(inplace=False, drop=True)
+    data = data.iloc[1:-1].reset_index(drop=True, inplace=False)
     #Quitar comas en columna de precios
-    #ERROR!!!!!
     data['Precio'] = [i.replace(',', '') for i in data['Precio']]
 
     data['Ticker'] = [i.replace('*', '') for i in data['Ticker']]
 
     #Hacer conversiones de tipos de columnas a numerico
-    #ERROR!!!!!
     convert_dict = {'Ticker': str, 'Nombre': str, 'Peso (%)': float, 'Precio': float}
     data = data.astype(convert_dict)
 
@@ -57,6 +57,7 @@ for i in archivos:
 
     #Guardar en diccionario
     data_archivos[i] = data
+
 # -----------------------------------------------------------------------------------------------------
 
 # Construir el vector de fechas a partir del vector de nombres
@@ -69,9 +70,9 @@ i_fechas = [j.strftime('%d%m%y') for j in sorted([pd.to_datetime(i[8:]).date() f
 # Descargar y acomodar datos
 tickers = []
 for i in archivos:
-    # i = archivos[11]
+    # i = archivos[0]
     l_tickers = list(data_archivos[i]['Ticker'])
-    print(l_tickers)
+    #print(l_tickers)
     [tickers.append(i + '.MX') for i in l_tickers]
 global_tickers = np.unique(tickers).tolist()
 
@@ -81,14 +82,28 @@ global_tickers = np.unique(tickers).tolist()
 global_tickers = [i.replace('LIVERPOLC.1.MX', 'LIVERPOLC-1.MX') for i in global_tickers]
 
 # eliminar MXN, USD, KOFL
-[global_tickers.remove(i) for i in ['MXN.MX', 'USD.MX']]
+[global_tickers.remove(i) for i in ['MXN.MX', 'USD.MX', 'KOFL.MX', 'KOFUBL.MX', 'BSMXB.MX']]
 
 #Nota: Cuando se utiliza KOF, ese % o ponderación la pasamos CASH
-
+#Contar tiempo que tarda
 inicio = time.time()
+#Descarga de precios de yfinance
 data = yf.download(global_tickers, start="2017-08-21", end="2020-08-21", actions=False, group_by="close", interval='1d',
                    auto_adjust=True, prepost=False, threads=False)
 print('se tardo', time.time() - inicio, 'segundos')
+
+#Convertir columna de fechas
+data_close = pd.DataFrame({i: data[i]['Close'] for i in global_tickers})
+
+#Tomar solo fechas de interes (Teoría de conjuntos)
+ic_fechas = sorted(list(set(data_close.index.astype(str).tolist()) & set(i_fechas)))
+
+#Localizar todos los precios
+precios = data_close.iloc[[int(np.where(data_close.index.astype(str) == i)[0]) for i in ic_fechas]]
+
+#Ordenar columnas lexicográficamente
+precios = precios.reindex(sorted(precios.columns), axis=1)
+
 
 #tomar solo las fechas de interes
 #tomar solo las columnas de interes
@@ -105,8 +120,59 @@ k = 1000000
 #comisiones por transaccion
 c = 0.00125
 
+#Vector de comisiones históricas
+comisiones = []
+
+#Obtener posición inicial (los % de KOFL, KOFUBL, BSMXB, USD asignarlos a CASH (eliminados))
+c_activos = ['KOFL', 'KOFUBL', 'BSMXB', 'MXN', 'USD']
+#Diccionario para resultado final
+inv_pasiva = {'timestamp': ['05-01-2018'], 'capital': [k]}
+
+#Falta revisar que el archivo sea del 1er mes
+pos_datos = data_archivos[archivos[0]].copy().sort_values('Ticker')[['Ticker', 'Nombre', 'Peso (%)']]
+
+#Extraer la lista de activos a eliminar
+i_activos = list(pos_datos[list(pos_datos['Ticker'].isin(c_activos))].index)
+
+#Eliminar los activos del dataframe
+pos_datos.drop(i_activos, inplace = True)
+
+#Resetear el index
+pos_datos.reset_index(inplace=True, drop = True)
+
+#Agregar .MX para empatar precios
+pos_datos['Ticker'] = pos_datos['Ticker'] + '.MX'
+
+#Corregir tickers en datos
+pos_datos['Ticker'] = pos_datos['Ticker'].replace('LIVEPOLC.1.MX', 'LIVEPOLC-1.MX')
+pos_datos['Ticker'] = pos_datos['Ticker'].replace('MEXCHEM.MX', 'ORBIA.MX')
+pos_datos['Ticker'] = pos_datos['Ticker'].replace('GFREGIOO.MX', 'RA.X')
+
+#Precios necesarios para la posición
+pos_datos['Precio'] = np.array(precios.iloc[0, [i in pos_datos['Ticker'].to_list() for i in
+                                                precios.columns.to_list()]])
+
+#Capital destinado por acción = proporción del capital - comisiones por la posición
+pos_datos['Capital'] = pos_datos['Peso (%)']*k - pos_datos['Peso (%)']*k*c
+
+#pos_datos['Titulos']
+
+#pos_datos['Titulos']
+
+#pos_datos['Postura']
+
+#pos_datos['Comisión']
+
+#pos_value
+
+#pos_comission
+
+
+
+
 data_archivos[archivos[0]]['Ticker']
 
 data_archivos[archivos[0]]['Peso (%)'] * (k//data_archivos[archivos[0]]['Peso (%)'])
+
 
 
