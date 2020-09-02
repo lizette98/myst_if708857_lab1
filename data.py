00e3f4b81f1cd9a14e76b85f1fdcdddb49cf575f
lizette98 <andreal98.acg@gmail.com>
2020-09-01 23:52:43 -0500

    sep1_changes

--- data.py
+++ data.py
@@ -22,11 +22,12 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-# Leer archivos
+#Leer archivos
 abspath = path.abspath('files/NAFTRAC_holdings')
 #Lista de todos los archivos en la carpeta
 #Comprensión de listas
 archivos = [f[:-4] for f in listdir(abspath) if isfile(join(abspath, f))]
+
 
 #Leer archivos y guardarlos en diccionario
 
@@ -34,30 +35,27 @@ archivos = [f[:-4] for f in listdir(abspath) if isfile(join(abspath, f))]
 data_archivos = {}
 
 for i in archivos:
-    i = archivos[0]
-    #Los lee después de los primeros dos renglones
-    data = pd.read_csv('files/NAFTRAC_holdings/'+ i + '.csv', skiprows=2, header=None)
-    #Renombrar columnas
+    #i = archivos[0]
+    # Los lee después de los primeros dos renglones
+    data = pd.read_csv('files/NAFTRAC_holdings/' + i + '.csv', skiprows=2, header=None)
+    # Renombrar columnas
     data.columns = list(data.iloc[0, :])
-    #Quitar columnas que no sean nan
+    # Quitar columnas que no sean nan
     data = data.loc[:, pd.notnull(data.columns)]
-    #Quitar renglon repetido y resetear índice, que empiece en 0
+    # Quitar renglon repetido y resetear índice, que empiece en 0
     data = data.iloc[1:-1].reset_index(drop=True, inplace=False)
-    #Quitar comas en columna de precios
+    # Quitar comas en columna de precios
     data['Precio'] = [i.replace(',', '') for i in data['Precio']]
 
     data['Ticker'] = [i.replace('*', '') for i in data['Ticker']]
-
-    #Hacer conversiones de tipos de columnas a numerico
+    # Hacer conversiones de tipos de columnas a numerico
     convert_dict = {'Ticker': str, 'Nombre': str, 'Peso (%)': float, 'Precio': float}
     data = data.astype(convert_dict)
 
-    #Convertir a decimal la columna de peso (%)
+    # Convertir a decimal la columna de peso (%)
     data['Peso (%)'] = data['Peso (%)']/100
-
-    #Guardar en diccionario
+    # Guardar en diccionario
     data_archivos[i] = data
-
 # -----------------------------------------------------------------------------------------------------
 
 # Construir el vector de fechas a partir del vector de nombres
@@ -65,21 +63,21 @@ for i in archivos:
 t_fechas = [i.strftime('%d-%m-%Y') for i in sorted([pd.to_datetime(i[8:]).date() for i in archivos])]
 
 # lista con fechas ordenadas (para usarse como  indexadores de archivos)
-i_fechas = [j.strftime('%d%m%y') for j in sorted([pd.to_datetime(i[8:]).date() for i in archivos])]
+i_fechas = [j.strftime('%Y-%m-%d') for j in sorted([pd.to_datetime(i[8:]).date() for i in archivos])]
 
 # Descargar y acomodar datos
 tickers = []
 for i in archivos:
-    # i = archivos[0]
+    # i = archivos[1]
     l_tickers = list(data_archivos[i]['Ticker'])
-    #print(l_tickers)
+    print(l_tickers)
     [tickers.append(i + '.MX') for i in l_tickers]
 global_tickers = np.unique(tickers).tolist()
 
 # Obtener posiciones históricas
-#global_tickers = [i.replace('GFREGIOO.MX', 'RA.MX') for i in global_tickers]
-#global_tickers = [i.replace('MEXCHEM.MX', 'ORBIA.MX') for i in global_tickers]
-global_tickers = [i.replace('LIVERPOLC.1.MX', 'LIVERPOLC-1.MX') for i in global_tickers]
+global_tickers = [i.replace('GFREGIOO.MX', 'RA.MX') for i in global_tickers]
+global_tickers = [i.replace('MEXCHEM.MX', 'ORBIA.MX') for i in global_tickers]
+global_tickers = [i.replace('LIVEPOLC.1.MX', 'LIVEPOLC-1.MX') for i in global_tickers]
 
 # eliminar MXN, USD, KOFL
 [global_tickers.remove(i) for i in ['MXN.MX', 'USD.MX', 'KOFL.MX', 'KOFUBL.MX', 'BSMXB.MX']]
@@ -89,8 +87,8 @@ global_tickers = [i.replace('LIVERPOLC.1.MX', 'LIVERPOLC-1.MX') for i in global_
 inicio = time.time()
 #Descarga de precios de yfinance
 data = yf.download(global_tickers, start="2017-08-21", end="2020-08-21", actions=False, group_by="close", interval='1d',
-                   auto_adjust=True, prepost=False, threads=False)
-print('se tardo', time.time() - inicio, 'segundos')
+                   auto_adjust=False, prepost=False, threads=False)
+print('se tardó', time.time() - inicio, 'segundos')
 
 #Convertir columna de fechas
 data_close = pd.DataFrame({i: data[i]['Close'] for i in global_tickers})
@@ -103,7 +101,6 @@ precios = data_close.iloc[[int(np.where(data_close.index.astype(str) == i)[0]) f
 
 #Ordenar columnas lexicográficamente
 precios = precios.reindex(sorted(precios.columns), axis=1)
-
 
 #tomar solo las fechas de interes
 #tomar solo las columnas de interes
@@ -128,7 +125,7 @@ c_activos = ['KOFL', 'KOFUBL', 'BSMXB', 'MXN', 'USD']
 #Diccionario para resultado final
 inv_pasiva = {'timestamp': ['05-01-2018'], 'capital': [k]}
 
-#Falta revisar que el archivo sea del 1er mes
+###----FALTA revisar que el archivo sea del 1er mes
 pos_datos = data_archivos[archivos[0]].copy().sort_values('Ticker')[['Ticker', 'Nombre', 'Peso (%)']]
 
 #Extraer la lista de activos a eliminar
@@ -155,15 +152,23 @@ pos_datos['Precio'] = np.array(precios.iloc[0, [i in pos_datos['Ticker'].to_list
 #Capital destinado por acción = proporción del capital - comisiones por la posición
 pos_datos['Capital'] = pos_datos['Peso (%)']*k - pos_datos['Peso (%)']*k*c
 
-#pos_datos['Titulos']
+#Cantidad de títulos por acción
+pos_datos['Titulos'] = pos_datos['Capital']//pos_datos['Precio']
 
 #pos_datos['Titulos']
+#Calcular los Títulos a comprar de cada activo
 
 #pos_datos['Postura']
+#Multiplicar los títulos de cada activo por el precio que tienes en ese mes
 
 #pos_datos['Comisión']
+#Calcular la comisión que pagas por ejecutar la postura
+
+#Efectivo libre en la posición
+#Capital - postura - comisión
 
 #pos_value
+
 
 #pos_comission
 
